d0cae82f445679d185fb180c0bd722a905f39805
h4z3m <h4z3m>
2021-12-17 00:18:18 +0200

    Creation

---
+++ Mars_Exploration_Project/main.cpp
@@ -0,0 +1,26 @@
+/*
+*                     Mars Exploration Project
+* 
+* Team members: 
+* 1.
+* 2.
+* 3.
+* 4.
+* 
+*/
+
+/*
+* File: main.cpp 
+* 
+* Description: Includes main function which executes the simulation
+* 
+* Author(s): 
+* 
+* Date Created:  17/12/2021
+*/
+int main(void) {
+
+
+
+	return 0;
+}/*End main*/
