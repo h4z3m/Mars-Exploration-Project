eac56494db4a7443aa0f1b86b82cce6fd29cf37f
AliKhalaf1 <47224980+AliKhalaf1@users.noreply.github.com>
2021-12-23 13:34:13 +0200

    added rover

--- Mars_Exploration_Project/Rovers/Rover.cpp
+++ Mars_Exploration_Project/Rovers/Rover.cpp
@@ -1,1 +1,67 @@
 #include "Rover.h"
+
+
+
+Rover::Rover(int input_checkup_duration, int input_speed, char input_rover_type, int input_initial_time_till_checkup)
+{
+	checkup_duration = input_checkup_duration;
+	speed = input_speed;
+	rover_type = input_rover_type;
+	initial_time_till_checkup = input_initial_time_till_checkup;
+	actual_time_till_checkup = input_initial_time_till_checkup;
+
+}
+
+void Rover::set_speed(int input_speed)
+{
+	speed = input_speed;
+}
+
+int Rover::get_speed()
+{
+	return speed;
+}
+
+void Rover::set_rover_type(int input_rover_type)
+{
+	rover_type = input_rover_type;
+}
+
+char Rover::get_rover_type()
+{
+	return rover_type;
+}
+
+void Rover::set_checkup_duration(int input_checkup_duration)
+{
+	checkup_duration = input_checkup_duration;
+}
+
+int Rover::get_checkup_duration()
+{
+	return checkup_duration;
+}
+
+void Rover::set_initial_time_till_checkup(int input_initial_time)
+{
+	initial_time_till_checkup = input_initial_time;
+	actual_time_till_checkup = input_initial_time;
+}
+
+int Rover::get_actual_time_till_checkup()
+{
+	return actual_time_till_checkup;
+}
+
+void Rover::decrement_actual_time_till_checkup()
+{
+	actual_time_till_checkup--;
+}
+
+void Rover::reset_actual_time_till_checkup()
+{
+	actual_time_till_checkup = initial_time_till_checkup;
+}
+
+
+
--- Mars_Exploration_Project/Rovers/Rover.h
+++ Mars_Exploration_Project/Rovers/Rover.h
@@ -1,5 +1,27 @@
 #pragma once
 class Rover
 {
+private:
+	int checkup_duration;
+	int speed;
+	char rover_type;
+	int initial_time_till_checkup;
+	int actual_time_till_checkup;
+public:
+	Rover(int input_checkup_duration,int input_speed,char input_rover_type,int input_initial_time_till_checkup);
+	void set_speed(int input_speed);
+	int get_speed();
+
+	void set_rover_type(int input_rover_type);
+	char get_rover_type();
+
+	void set_checkup_duration(int input_checktup_duration);
+	int get_checkup_duration();
+
+	void set_initial_time_till_checkup(int input_initial_time);       //set initial and actual time to checkup
+
+	int get_actual_time_till_checkup();
+	void decrement_actual_time_till_checkup();
+	void reset_actual_time_till_checkup();
 };
 
