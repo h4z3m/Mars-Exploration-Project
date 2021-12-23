bc49549120ed46118e11ed8f95ad249e64e8793a
h4z3m <h4z3m>
2021-12-23 15:39:31 +0200

    Test

--- Mars_Exploration_Project/Misc/Station.cpp
+++ Mars_Exploration_Project/Misc/Station.cpp
@@ -7,6 +7,32 @@ Station::Station()
 	//TODO Load from i/p file
 	
 
+
+
+}
+
+bool IO::IO_ReadFile(PriQ<Event>*& ReturnList)
+{
+
+	UI_printString((string*)"Enter filename (including ext): ");
+	filename = UI_getString();
+
+	file.open(fileName);
+	//Failed to open file
+	if (!file.is_open()) {
+		return false;
+	}
+	else {
+		//Start loading data into event lists
+
+
+
+	}
+}
+
+bool IO::IO_OutputFile(PriQ<Event>*& OutputList)
+{
+	return false;
 }
 
 Station::~Station()
--- Mars_Exploration_Project/Misc/Station.h
+++ Mars_Exploration_Project/Misc/Station.h
@@ -1,22 +1,43 @@
 #pragma once
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include"UI.h"
 #include "../Rovers/Rover.h"
 #include "../Datastructures/Queue ADT/LinkedQueue.h"
-
 #include "../Datastructures/PriQ ADT/PriQ.h"
 class Station
 {
 private:
+	UI App;
+	ifstream file;
+	string fileName;
+	
 	/// REMOVE WHEN EVENTS AND MISSIONS ARE COMPLETED
 	typedef int Event;
 	typedef int Mission;
-
 	/// </summary>
 	uint16_t current_day;
-	LinkedQueue<int> d;
-	PriQ<Rover*> InCheckupPolarRover;
+
+	LinkedQueue<Event*> Events;
+
+	PriQ<Mission*> InExecutionMissions;
+
+	PriQ<Rover*> PolarRovers;
+	PriQ<Rover*> EmergencyRovers;
+	PriQ<Rover*> MountainRovers;
+	
+	LinkedQueue<Rover*> InCheckupPolarRovers;
+	LinkedQueue<Rover*> InCheckupEmergencyRovers;
+	LinkedQueue<Rover*> InCheckupMountainRovers;
+	//Private Utility Functions
+
+	bool IO_ReadFile(PriQ<Event>*& ReturnList);
+	bool IO_OutputFile(PriQ<Event>*& OutputList);
 	public:
 	Station();
+	//Public Member Functions
 	~Station();
 
 
