9c417a028d9bf0a8f81f822690ed3a0063dbd9ee
AliKhalaf1 <47224980+AliKhalaf1@users.noreply.github.com>
2021-12-24 18:14:52 +0200

    unknown

---
+++ Mars_Exploration_Project/Misc/common_types.h
@@ -0,0 +1,15 @@
+#pragma once
+/*Unsigned numbers*/
+typedef unsigned int   uint32;	/* 0              - 4,294,967,295  */
+typedef unsigned short uint16;  /* 0              - 65,535		   */
+typedef unsigned char  uint8;   /* 0              - 255			   */
+/*Signed numbers*/
+typedef signed int sint32;		/*-2,147,483,648  - +2,147,483,647 */
+typedef signed int sint16;		/* -32,768        - +32,767	       */
+typedef signed int sint8;		/*-128            - +127		   */
+
+enum Rover_Types {
+	Emergency = 'E',
+	Polar	  = 'P',
+	Mountain  = 'M'
+};
