e4bb3464cfc31deb7535cee171effd8a3a7c2bb1
h4z3m <h4z3m>
2022-01-06 01:07:36 +0200

    UI functions

--- Mars_Exploration_Project/Misc/ui.h
+++ Mars_Exploration_Project/Misc/ui.h
@@ -1,19 +1,25 @@
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <ostream>
+#include <chrono>
+#include <thread>
+#include "common_types.h"
 #include "../Missions/Mission.h"
 #include "../Rovers/Polar_Rover.h"
+
+#define CLEAR_STR(name) name.str("");name.clear()
 using namespace std;
 class UI {
 private:
-
 public:
 	void UI_printString(const char *st);
+	void UI_printStringColor(const char* COLOR_CODE,stringstream& msg);
 	string UI_getString();
 	char UI_getChar();
-	void UI_Interactive_Mode(void);
-	void UI_Step_By_Step(void);
-	void UI_Silent_Mode(void);
 	void print_rover_info(Rover *r);
 	void print_mission_info(Mission *m);
-
+	void UI_WaitKeyPress();
+	void UI_DelaySeconds(int sec);
+	void UI_clear_screen();
 };
